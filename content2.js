32a9c9d32d19c9dc0be9f2ef787313b208bc9976
len <l.yoshida.132@gmail.com>
2021-10-01 00:34:38 +0900

    test implement

--- content2.js
+++ content2.js
@@ -2,31 +2,10 @@ const body = document.querySelector('html');
 const panel = document.createElement('div');
 const msg = document.createTextNode(time2str());
 
-panel.style.fontSize = 'large';
-panel.style.position = 'fixed';
-panel.style.bottom = '10%';
-panel.style.left = '50%';
-panel.style.transform = 'translateX(-50%)';
-panel.style.background = 'rgba(0,0,0,0.5)';
-panel.style.color = 'white';
+const table = document.getElementById('punchclocktable');
+const indices= table.querySelectorAll('.celda_encabezado_general');
 
-panel.appendChild(msg);
-body.appendChild(panel);
-
-function time2str(){
-    let now = new Date();
-    let hour = now.getHours();
-    let min = now.getMinutes();
-    let sec = now.getSeconds();
-    return pad0(hour) + ':' + pad0(min) + ':' + pad0(sec);
+for (let i = 0; i < indices.length; i++){
+    indices[i].setAttribute('onclick', 'alert("aaa")')
 }
 
-function pad0(number){
-    return ('0' + number).slice(-2);
-}
-
-function updateTime(){
-    msg.textContent = time2str();
-}
-
-setInterval('updateTime()', 1000);
