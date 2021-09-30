de3a1137d748275190d505a7522e1cf51fafae79
len <l.yoshida.132@gmail.com>
2021-10-01 00:44:00 +0900

    test implement

--- content2.js
+++ content2.js
@@ -1,11 +1,12 @@
 const body = document.querySelector('html');
 const panel = document.createElement('div');
-const msg = document.createTextNode(time2str());
 
 const table = document.getElementById('punchclocktable');
 const indices= table.querySelectorAll('.celda_encabezado_general');
 
+alert(indices.length);
+
 for (let i = 0; i < indices.length; i++){
-    indices[i].setAttribute('onclick', 'alert("aaa")')
+    indices[i].setAttribute('onclick', 'alert("aaa")');
 }
 
