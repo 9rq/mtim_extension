75a4012e70eaae94dd20539c71e9506b9fd25453
len <l.yoshida.132@gmail.com>
2021-10-04 10:32:18 +0900

    refactored

--- confirm_mod.js
+++ confirm_mod.js
@@ -3,11 +3,12 @@ const tbody = table.getElementsByTagName('tbody').item(0);
 const lines = tbody.getElementsByTagName('tr');
 const indices = document.querySelectorAll('.celda_encabezado_general');
 
-let times = ['09:00', '12:00', '12:00', '13:00', '13:00', '18:00']
+
+let times = ['09:00', '12:00', '12:00', '13:00', '13:00', '18:00'];
 
 
 function handler(i){
-    for (let j=0; j < 6; j++){
+    for (let j=0; j < times.length; j++){
         lines.item(j).getElementsByTagName('input').item(i).setAttribute('value', times[j]);
     }
 }
