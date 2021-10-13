be3bb45bebe3cbda91bc75ad4a85123a24da5f55
len <l.yoshida.132@gmail.com>
2021-10-13 21:29:14 +0900

    refactored

--- src/background.js
+++ src/background.js
@@ -1,20 +1,21 @@
 // to decide in which website popup is available
 new Promise(resolve => {
-  chrome.runtime.onInstalled.addListener(resolve)
+    chrome.runtime.onInstalled.addListener(resolve)
 }).then(() => {
-  return new Promise(resolve => {
-    chrome.declarativeContent.onPageChanged.removeRules(undefined, resolve)
-  })
+    return new Promise(resolve => {
+        chrome.declarativeContent.onPageChanged.removeRules(undefined, resolve)
+    })
 }).then(() => {
-  chrome.declarativeContent.onPageChanged.addRules([{
-    conditions: [
-      new chrome.declarativeContent.PageStateMatcher({
-        pageUrl: {
-          hostEquals: 'developer.mozilla.org',
-          schemes: ['https']
-        }
-      })
-    ],
-    actions: [new chrome.declarativeContent.ShowPageAction()]
-  }])
+    chrome.declarativeContent.onPageChanged.addRules([{
+        conditions: [
+            new chrome.declarativeContent.PageStateMatcher({
+                pageUrl: {
+                    hostEquals: 'whm.accenture.com',
+                    pathEquals: '/mytim/secure/punchClock/confirm',
+                    schemes: ['https']
+                }
+            })
+        ],
+        actions: [new chrome.declarativeContent.ShowPageAction()]
+    }])
 })
