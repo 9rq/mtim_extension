9b438553d7dc76df4b39d9617dba7f02904063fa
len <l.yoshida.132@gmail.com>
2021-10-20 12:09:42 +0900

    add contextMenu to access mtim easily

--- src/background.js
+++ src/background.js
@@ -19,3 +19,45 @@ new Promise(resolve => {
         actions: [new chrome.declarativeContent.ShowPageAction()]
     }])
 })
+
+
+// contextMenu
+// parent
+chrome.contextMenus.create({
+    id: "mtim",
+    title: "open myTIM",
+    contexts: ["page_action"],
+    onclick: ()=>{
+        chrome.tabs.create({url:"https://whm.accenture.com/mytim/secure/punchClock"});
+    }
+});
+// working
+//chrome.contextMenus.create({
+//    id: "mtim_work",
+//    title: "start working",
+//    contexts: ["page_action"],
+//    parentId: "mtim",
+//    onclick: ()=>{
+//        chrome.tabs.create({url:"https://google.com"});
+//    }
+//});
+//// resting
+//chrome.contextMenus.create({
+//    id: "mtim_rest",
+//    title: "start resting",
+//    contexts: ["page_action"],
+//    parentId: "mtim",
+//    onclick: ()=>{
+//        chrome.tabs.create({url:"https://google.com"});
+//    }
+//});
+//// finish
+//chrome.contextMenus.create({
+//    id: "mtim_finish",
+//    title: "finsh resting",
+//    contexts: ["page_action"],
+//    parentId: "mtim",
+//    onclick: ()=>{
+//        chrome.tabs.create({url:"https://google.com"});
+//    }
+//});
